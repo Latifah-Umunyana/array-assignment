a8841905b860f1af643ffeb233bd9422496059fa
Latifa-Umunyana <latifahumunyana@gmail.com>
2024-03-22 19:58:31 +0300

    add files

--- arrays.js
+++ arrays.js
@@ -7,8 +7,9 @@ console.log(arr2[4]);
 
 // Q2. Write a JS program that will join the following array elements into a stringmyPets = ["Cow", "Bird", "Snake", "Dog"];
 
+
 stringmyPets = ["Cow", "Bird", "Snake", "Dog"];
-let pets = stringmyPets.join();
+let pets = stringmyPets.join(' ');
 console.log(pets);
 
 // Q3. Write a JS script to sort the following array itemsvar arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
@@ -22,44 +23,21 @@ console.log(arr);
 let arra = [["boy", "man", "girl","school", "girl", "woman"]];
 
 
-arra.forEach{ x =>
-
-    
-
-
-}
-
-
-// let rmDuplicate = [];
-// let duplicate = [];
-// arra.forEach(f =>{
-//     if (rmDuplicate.includes(f)){ 
-//         rmDuplicate.push(f); 
-//     }
-//     else(duplicate.push(f));
-// });
-// console.log({rmDuplicate});
-// console.log({duplicate});
 
 // Q5. Write a JS script to search for the following word in the array."food"If the word is present, return it else return "the search word was not found"let arr5 = ["the", "way", "x", 4];
 
-let look = ["the", "way", "x", 4];
-let word = "way";
-let textIndex = look.indexOf(word);
-if (word== look["way"]){
-    console.log({word})
+let arr5 = ["the","way","x",4];
+let k = arr5.includes("food");
+if(k==true){
+    console.log("food");
 }
+
 else{
-    console.log("The search was not found ")
+    console.log("the search word was not found");
 }
 
 //Q6. Write a JS script to sort the following string:let word = "renniw"
 
-let wrd=  "renniw";
-function newSort (word) {
-    return word.split("")
-           word.sort()
-            word.join("");
-}
-console.log(newSort(wrd));
-
+let reverseWord = "renniw";
+let z = reverseWord.split("").reverse().join("");
+console.log({z});
